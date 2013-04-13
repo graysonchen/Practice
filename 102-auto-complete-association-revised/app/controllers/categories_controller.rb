819106b389795aa6a88b24ba14406d78c8a1a22e
John.Grayson <cgg5207@sina.com>
2013-04-14 00:52:12 +0800

    auto complete

---
+++ 102-auto-complete-association-revised/app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApplicationController
+
+
+  def index
+  	@categories = Category.order(:name).where("name like ?","%#{params[:term]}%")
+  	render json: @categories.map(&:name)
+  end
+end
